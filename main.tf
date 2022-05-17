ba7b34c873494f6309040306031b9c372c352835
Per Næss <perness3009@gmail.com>
2022-05-17 16:27:57 +0200

    fix: remove unsupported block

--- main.tf
+++ main.tf
@@ -18,10 +18,4 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   identity {
     type = "SystemAssigned"
   }
-
-  addon_profile {
-    http_application_routing {
-      enabled = true
-    }
-  }
 }
