0ba0e365c596a21f2f85dd9ef3cd3bc34b7aa787
Per Næss <perness3009@gmail.com>
2022-05-17 18:07:22 +0200

    fix: format

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   kubernetes_version = "1.25"
 
   open_service_mesh_enabled = false
-  private_cluster_enabled = false
+  private_cluster_enabled   = false
 
   private_dns_zone_id = "System"
 
@@ -28,31 +28,31 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
   auto_scaler_profile {
     balance_similar_node_groups = false
-    expander = "least-waste"
-    scale_down_delay_after_add = "5m"
+    expander                    = "least-waste"
+    scale_down_delay_after_add  = "5m"
   }
 
   default_node_pool {
-    name       = "default"
-    vm_size    = "standard_B2s"
+    name    = "default"
+    vm_size = "standard_B2s"
 
     enable_auto_scaling = false
-    node_count = "1"
-    max_pods = 125
+    node_count          = "1"
+    max_pods            = 125
 
     os_disk_size = "32gb"
     os_disk_type = "Ephemeral"
 
     pod_subnet_id = data.azurerm_subnet.subnet1.id
 
-    type = "VirtualMachineScaleSets"
+    type              = "VirtualMachineScaleSets"
     ultra_ssd_enabled = false
   }
 
   network_profile {
-    network_plugin = "azure"
-    network_mode = "transparent"
-    network_policy = "calico"
+    network_plugin    = "azure"
+    network_mode      = "transparent"
+    network_policy    = "calico"
     load_balancer_sku = "basic"
   }
 
