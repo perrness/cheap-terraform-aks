ddb91658c13690cbe28c478fd43850efbb661f97
Per Næss <perness3009@gmail.com>
2022-07-16 21:07:39 +0200

    fix(network): set dns server to managed

--- network/main.tf
+++ network/main.tf
@@ -14,7 +14,6 @@ resource "azurerm_virtual_network" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
   subnet {
     name           = "${var.vnet_name}-subnet-1"
