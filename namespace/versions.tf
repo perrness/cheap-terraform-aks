1662a63c73461a78b375518f7e949571a8a5278f
Per Næss <perness3009@gmail.com>
2022-07-17 11:04:13 +0200

    fix(namespace): add azure provider

--- namespace/versions.tf
+++ namespace/versions.tf
@@ -4,13 +4,20 @@ terraform {
   }
 
   required_providers {
-    required_providers {
-      kubernetes = {
-        source  = "hashicorp/kubernetes"
-        version = "2.12.1"
-      }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.12.1"
     }
+    # azurerm = {
+    #   source  = "hashicorp/azurerm"
+    #   version = "=3.14.0"
+    # }
   }
 }
 
 provider "kubernetes" {}
+
+# provider "azurerm" {
+#   use_oidc = true
+#   features {}
+# }
