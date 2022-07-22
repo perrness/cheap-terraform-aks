b653bbeea108a024d053fb9566acc57ae036a0c9
Per Næss <perness3009@gmail.com>
2022-07-22 18:18:47 +0200

    feat(helm/linkerd): add automatic tls credential rotation

--- helm/modules/linkerd/linkerd.tf
+++ helm/modules/linkerd/linkerd.tf
@@ -25,7 +25,7 @@ resource "tls_private_key" "linkerd_issuer" {
   ecdsa_curve = "P256"
 }
 
-resource "tls_private_key" "linkerd_webhook_issuer" {
+resource "tls_private_key" "linkerd_webhook" {
   algorithm   = "ECDSA"
   ecdsa_curve = "P256"
 }
@@ -76,15 +76,15 @@ resource "tls_self_signed_cert" "linkerd_issuer" {
   ]
 }
 
-resource "tls_self_signed_cert" "linkerd_webhook_issuer" {
-  private_key_pem = tls_private_key.linkerd_webhook_issuer.private_key_pem
+resource "tls_self_signed_cert" "linkerd_webhook" {
+  private_key_pem = tls_private_key.linkerd_webhook.private_key_pem
 
   subject {
-    common_name = "identity.linkerd.cluster.local" #TODO
+    common_name = "webhook.linkerd.cluster.local"
   }
 
   validity_period_hours = 168
-  early_renewal_hours   = 24 * 30 * 11
+  early_renewal_hours   = 154
   is_ca_certificate     = true
 
   allowed_uses = [
@@ -95,7 +95,7 @@ resource "tls_self_signed_cert" "linkerd_webhook_issuer" {
   ]
 
   depends_on = [
-    tls_private_key.linkerd_issuer
+    tls_private_key.linkerd_webhook
   ]
 }
 
@@ -171,39 +171,109 @@ resource "kubernetes_manifest" "linkerd_trust_anchor_certificate" {
   ]
 }
 
-resource "kubernetes_secret" "linkerd_webhook_issuer" {
+resource "kubernetes_secret" "linkerd_webhook" {
   metadata {
-    name      = "linkerd-webhook-issuer"
+    name      = "webhook-issuer-tls"
     namespace = var.namespace
   }
 
   type = "kubernetes.io/tls"
   data = {
-    "tls.crt" = tls_self_signed_cert.linkerd_trust_anchor.cert_pem
-    "tls.key" = tls_private_key.linkerd_trust_anchor.private_key_pem # key used to generate Certificate Request
+    "tls.crt" = tls_self_signed_cert.linkerd_webhook.cert_pem
+    "tls.key" = tls_private_key.linkerd_webhook.private_key_pem # key used to generate Certificate Request
   }
 
   depends_on = [
-    tls_private_key.linkerd_webhook_issuer
+    tls_private_key.linkerd_webhook
   ]
 }
 
-resource "kubernetes_manifest" "linkerd_webhook_issuer_certificate" {
+resource "kubernetes_manifest" "linkerd_webhook" {
   manifest = {
     apiVersion = "cert-manager.io/v1"
     kind       = "Issuer"
     metadata = {
-      name      = "linkerd-webhook-issuer"
+      name      = "webhook-issuer"
       namespace = var.namespace
     }
     spec = {
       ca = {
-        secretName = "linkerd-webhook-issuer"
+        secretName = "webhook-issuer-tls"
       }
     }
   }
   depends_on = [
-    kubernetes_secret.linkerd_webhook_issuer
+    kubernetes_secret.linkerd_webhook
+  ]
+}
+
+resource "kubernetes_manifest" "linkerd_policy_validator" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Certificate"
+    metadata = {
+      name      = "linkerd-policy-validator"
+      namespace = "linkerd"
+    }
+    spec = {
+      secretName  = "linkerd-policy-validator-k8s-tls"
+      duration    = "24h0m0s"
+      renewBefore = "1h0m0s"
+      issuerRef = {
+        name = "webhook-issuer"
+        kind = "Issuer"
+      }
+      commonName = "linkerd-policy-validator.linkerd.svc"
+      dnsNames = [
+        "linkerd-policy-validator.linkerd.svc"
+      ]
+      privateKey = {
+        algorithm = "ECDSA"
+        encoding  = "PKCS8"
+      }
+      usages = [
+        "server auth"
+      ]
+    }
+  }
+
+  depends_on = [
+    kubernetes_manifest.linkerd_webhook
+  ]
+}
+
+resource "kubernetes_manifest" "linkerd_proxy_injector" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Certificate"
+    metadata = {
+      name      = "linkerd-proxy-injector"
+      namespace = "linkerd"
+    }
+    spec = {
+      secretName  = "linkerd-proxy-injector-k8s-tls"
+      duration    = "24h0m0s"
+      renewBefore = "1h0m0s"
+      issuerRef = {
+        name = "webhook-issuer"
+        kind = "Issuer"
+      }
+      commonName = "linkerd-proxy-injector.linkerd.svc"
+      dnsNames = [
+        "linkerd-proxy-injector.linkerd.svc"
+      ]
+      isCA = false
+      privateKey = {
+        algorithm = "ECDSA"
+      }
+      usages = [
+        "server auth"
+      ]
+    }
+  }
+
+  depends_on = [
+    kubernetes_manifest.linkerd_webhook
   ]
 }
 
@@ -218,15 +288,16 @@ resource "kubernetes_manifest" "linkerd_sp_validator_certificate" {
     spec = {
       secretName  = "linkerd-sp-validator-k8s-tls"
       duration    = "24h0m0s"
-      renewBefore = "12h0m0s"
+      renewBefore = "1h0m0s"
       issuerRef = {
-        name = "linkerd-webhook-issuer"
+        name = "webhook-issuer"
         kind = "Issuer"
       }
       commonName = "linkerd-sp-validator.linkerd.svc"
       dnsNames = [
         "linkerd-sp-validator.linkerd.svc"
       ]
+      isCA = false
       privateKey = {
         algorithm = "ECDSA"
       }
@@ -237,7 +308,7 @@ resource "kubernetes_manifest" "linkerd_sp_validator_certificate" {
   }
 
   depends_on = [
-    kubernetes_manifest.linkerd_webhook_issuer_certificate
+    kubernetes_manifest.linkerd_webhook
   ]
 }
 
@@ -282,12 +353,22 @@ resource "helm_release" "linkerd" {
 
   set {
     name  = "proxyInjector.caBundle"
-    value = tls_self_signed_cert.linkerd_webhook_issuer.cert_pem
+    value = tls_self_signed_cert.linkerd_webhook.cert_pem
+  }
+
+  set {
+    name  = "policyValidator.externalSecret"
+    value = "true"
+  }
+
+  set {
+    name  = "policyValidator.caBundle"
+    value = tls_self_signed_cert.linkerd_webhook.cert_pem
   }
 
   set {
     name  = "profileValidator.caBundle"
-    value = tls_self_signed_cert.linkerd_webhook_issuer.cert_pem
+    value = tls_self_signed_cert.linkerd_webhook.cert_pem
   }
 
   set {
@@ -298,7 +379,7 @@ resource "helm_release" "linkerd" {
   depends_on = [
     kubernetes_manifest.linkerd_trust_anchor_certificate,
     tls_self_signed_cert.linkerd_issuer,
-    kubernetes_manifest.linkerd_webhook_issuer_certificate,
-    tls_self_signed_cert.linkerd_webhook_issuer
+    kubernetes_manifest.linkerd_webhook,
+    tls_self_signed_cert.linkerd_webhook
   ]
 }
