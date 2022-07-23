a88b5b1d4ba5d40cbc200831fe978d244e6e60ec
Per Næss <perness3009@gmail.com>
2022-07-23 19:22:42 +0200

    fix(network): try another address space

--- cluster/main.tf
+++ cluster/main.tf
@@ -69,7 +69,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     network_policy     = "azure"
     load_balancer_sku  = "basic"
     docker_bridge_cidr = "172.17.0.1/16"
-    service_cidr       = "10.0.0.0/24"
+    service_cidr       = "10.0.0.0/16"
     dns_service_ip     = "10.0.0.10"
     ip_versions        = ["IPv4"]
   }
