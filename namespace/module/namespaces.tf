e922b26110f76ad1faaecab4da5836ead38514d3
Per Næss <perness3009@gmail.com>
2022-07-27 12:40:56 +0200

    feat(helm/linkerd): added linkerd annotations to non linkerd namesapces

--- namespace/module/namespaces.tf
+++ namespace/module/namespaces.tf
@@ -1,8 +1,8 @@
 resource "kubernetes_namespace" "namespace" {
   metadata {
-    # annotations = {
-    #   "linkerd.io/inject" = "enabled"
-    # }
+    annotations = {
+      "linkerd.io/inject" = length(regexall("linkerd", var.name)) == 0 ? "enabled" : "disabled"
+    }
 
     labels = {
       release = "kube-prometheus-stack"
